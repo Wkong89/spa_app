db62ff21966ece61f18165adba406ddb2e27ebb4
WOO-BIN LEE <iopplm89@gmail.com>
2022-12-13 17:10:14 +0900

    Single Page APp 예제 추가함

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+// pages/Home.js
+
+const Home= ()=>{
+
+    const homeStyle={
+        backgroundColor:"pink",
+        height:"300px"
+    };
+
+    return(
+        <div style={homeStyle}>
+            <h3>home page 입니다.</h3>
+            <h3>하나</h3>
+        </div>
+    )
+}
+
+export default Home;
